49d2dd46be7d356202ce56ebd4651d6c16f463f2
Thanmaireddy03 <tbvsma@charlotte.edu>
2024-09-17 22:57:45 +0000

    Completed Movie Script Analysis Assignment

--- src/main/java/com/movie/script/analysis/MovieScriptAnalysis.java
+++ src/main/java/com/movie/script/analysis/MovieScriptAnalysis.java
@@ -14,15 +14,15 @@ public class MovieScriptAnalysis {
         Configuration conf = new Configuration();
 
         // Task 1: Most Frequent Words by Character
-        Job job1 = Job.getInstance(conf, "Most Frequent Words by Character");
-        job1.setJarByClass(MovieScriptAnalysis.class);
-        job1.setMapperClass(CharacterWordMapper.class);
-        job1.setReducerClass(CharacterWordReducer.class);
-        job1.setOutputKeyClass(Text.class);
-        job1.setOutputValueClass(IntWritable.class);
-        FileInputFormat.addInputPath(job1, new Path(args[1]));
-        FileOutputFormat.setOutputPath(job1, new Path(args[2] + "/task1"));
-        job1.waitForCompletion(true);
+        //Job job1 = Job.getInstance(conf, "Most Frequent Words by Character");
+        //job1.setJarByClass(MovieScriptAnalysis.class);
+        //job1.setMapperClass(CharacterWordMapper.class);
+        //job1.setReducerClass(CharacterWordReducer.class);
+        //job1.setOutputKeyClass(Text.class);
+        //job1.setOutputValueClass(IntWritable.class);
+        //FileInputFormat.addInputPath(job1, new Path(args[1]));
+        //FileOutputFormat.setOutputPath(job1, new Path(args[2] + "/task1"));
+        //job1.waitForCompletion(true);
 
         // Task 2: Dialogue Length Analysis
         Job job2 = Job.getInstance(conf, "Dialogue Length Analysis");
@@ -36,14 +36,14 @@ public class MovieScriptAnalysis {
         job2.waitForCompletion(true);
 
         // Task 3: Unique Words by Character
-        Job job3 = Job.getInstance(conf, "Unique Words by Character");
-        job3.setJarByClass(MovieScriptAnalysis.class);
-        job3.setMapperClass(UniqueWordsMapper.class);
-        job3.setReducerClass(UniqueWordsReducer.class);
-        job3.setOutputKeyClass(Text.class);
-        job3.setOutputValueClass(Text.class);
-        FileInputFormat.addInputPath(job3, new Path(args[1]));
-        FileOutputFormat.setOutputPath(job3, new Path(args[2] + "/task3"));
-        System.exit(job3.waitForCompletion(true) ? 0 : 1);
+        //Job job3 = Job.getInstance(conf, "Unique Words by Character");
+        //job3.setJarByClass(MovieScriptAnalysis.class);
+        //job3.setMapperClass(UniqueWordsMapper.class);
+        //job3.setReducerClass(UniqueWordsReducer.class);
+        //job3.setOutputKeyClass(Text.class);
+        //job3.setOutputValueClass(Text.class);
+        //FileInputFormat.addInputPath(job3, new Path(args[1]));
+        //FileOutputFormat.setOutputPath(job3, new Path(args[2] + "/task3"));
+        //System.exit(job3.waitForCompletion(true) ? 0 : 1);
     }
 }
